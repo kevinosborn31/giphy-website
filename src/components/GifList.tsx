dd79a2b0732a48c93bd3f65a1c9841c81cd2724b
Kevin <kevinosborn31@gmail.com>
2023-08-31 13:49:58 +1000

    Added MUI package, created skeleton components

---
+++ src/components/GifList.tsx
@@ -0,0 +1,12 @@
+import { Box } from '@mui/material';
+
+const GifList = () => {
+
+  return (
+    <Box>
+        Gif list
+    </Box>
+  );
+};
+
+export default GifList;
---
+++ src/components/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+import { Box } from '@mui/material';
+import GifList from '../GifList';
+
+const HomePage = () => {
+
+  return (
+    <Box>
+        <GifList />
+    </Box>
+  );
+};
+
+export default HomePage;
